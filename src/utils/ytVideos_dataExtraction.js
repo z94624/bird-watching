8247365931e19fe156c0f806617a8a42f46050aa
smoBEE <z94624@gmail.com>
2021-06-14 21:08:24 +0800

    建立 YouTube 篩選賞鳥地點與鳥種下拉選單

--- src/utils/ytVideos_dataExtraction.js
+++ src/utils/ytVideos_dataExtraction.js
@@ -1,3 +1,15 @@
 import youTubeVideosData from './youtube-videos.json';
 
-export const 
+export const getItems = (key) => {
+	let items = youTubeVideosData.reduce((arr, ele) => 
+		arr.concat([ele[key]])
+	, []);
+	return new Set(items);
+}
+
+export const itemsToRainbowOptions = (items) => {
+	let allOption = { value: "*全部", label: "*全部" };
+	return [...items].reduce((arr, ele) => 
+		arr.concat([{ value: ele, label: ele }])
+	, [allOption]);
+}
--- src/views/YouTube.js
+++ src/views/YouTube.js
@@ -1,24 +1,48 @@
 import { useState } from 'react';
 
-import { DatePicker } from 'react-rainbow-components';
+import { DatePicker, Select } from 'react-rainbow-components';
+
+import { getItems, itemsToRainbowOptions } from './../utils/ytVideos_dataExtraction';
 
 const YouTube = () => {
 	const [dateRange, setDateRange] = useState(new Date());
 
+	const locations = getItems('location');
+	const locationOptions = itemsToRainbowOptions(locations);
+
+	const birds = getItems('bird');
+	const birdOptions = itemsToRainbowOptions(birds);
+
 	return (
-		<main>
+		<main className="h-100">
 			<div className="w-25 d-inline-block">
 				<DatePicker
-					selectionType="range"
-					placeholder="單一日期 or 日期區間"
-					value={dateRange}
-					minDate={new Date(2020, 10, 26)}
+					formatStyle="medium"
+					hideLabel={true}
+					id="ytDatePicker"
+					isCentered={true}
+					locale="tw"
 					maxDate={new Date(2040, 11, 25)} // 依照 YouTube 賞鳥紀錄最新影片日期
+					minDate={new Date(2020, 10, 26)}
 					onChange={value => setDateRange(value)}
+					placeholder="單一日期 或 日期區間"
+					selectionType="range"
+					value={dateRange}
 				/>
 			</div>
 			<div className="w-25 d-inline-block">
-				
+				<Select
+					hideLabel={true}
+					id="ytLocationSelect"
+					options={locationOptions}
+				/>
+			</div>
+			<div className="w-25 d-inline-block">
+				<Select
+					hideLabel={true}
+					id="ytBirdSelect"
+					options={birdOptions}
+				/>
 			</div>
 		</main>
 	);
