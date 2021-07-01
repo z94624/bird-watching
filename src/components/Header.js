19db6742103da27674a964d41651165f166c2c34
smoBEE <z94624@gmail.com>
2021-07-01 22:38:21 +0800

    1. 切換分頁後收合分頁列表 2. 分頁列表置中 3. 分頁列表改為 Hover 觸發。

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
-import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
+
+import './Header.css';
 
 import LogoIcon from './../images/logo.png';
 // 各分頁的名稱與路徑
@@ -16,12 +18,19 @@ const navList = [
 
 const Header = () => {
 	// 分頁列表選單的開合狀態
-	const [isNavCollapsed, setIsNavCollapsed] = useState(true);
-    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+	const [navCollapsed, setNavCollapsed] = useState(true);
+    // 偵測分頁轉換
+    const browseHistory = useHistory(); // 轉換路徑歷史
+    useEffect(() => {
+        // 若打開分頁列切換頁面後，立即收合分頁列
+        return browseHistory.listen(location => {
+            setNavCollapsed(true);
+        });
+    }, [browseHistory]);
 
 	return (
 		<header className="ps-3">
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                 <div className="container-fluid">
                     {/* 網頁品牌 */}
                     <NavLink className="navbar-brand float-md-start" to="/bird-watching">
@@ -29,18 +38,34 @@ const Header = () => {
                         <span className="ms-2">楓楓與小芬的島羽</span> {/* 品牌名稱 */}
                     </NavLink>
                     {/* 分頁列表的展開按鈕 */}
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTabs" aria-controls="navbarTabs" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
-                        <span className="navbar-toggler-icon"></span>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarTabs"
+                        aria-controls="navbarTabs"
+                        aria-expanded={navCollapsed ? false : true}
+                        aria-label="Toggle navigation"
+                        onMouseEnter={() => setNavCollapsed(false)}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                        </svg>
                     </button>
                     {/* 分頁列表 */}
-                    <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse ms-5`} id="navbarTabs">
+                    <div
+                        className={`navbar-collapse${navCollapsed ? ' collapse' : '  collapse show'}`}
+                        id="navbarTabs"
+                        onMouseLeave={() => setNavCollapsed(true)}
+                    >
                         <ul className="navbar-nav">
                         	{navList.map(({to, text}, idx) => (
                         		<li key={"navItem-"+idx} className="nav-item">
                         			<NavLink
                         				exact
                         				className="nav-link"
-                        				activeClassName="nav-link-active"
+                        				activeClassName="nav-link active"
+                                        aria-current="page"
                         				to={to} // 分頁路徑
                         			>
                         				{text} {/* 分頁名稱 */}
