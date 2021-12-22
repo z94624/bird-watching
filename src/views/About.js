fa0c7b3534c7862dd82257177ce72bc81851f7b6
smoBEE <z94624@gmail.com>
2021-12-22 22:23:27 +0800

    About: 1. 首頁設定響應式字體大小 2. 新增自我簡介區。

--- src/views/About.js
+++ src/views/About.js
@@ -1,4 +1,5 @@
 import AboutHome from './About_Home';
+import AboutMe from './About_Me';
 import './About.css';
 
 const About = () => {
@@ -6,6 +7,8 @@ const About = () => {
 		<main className="h-100 container-fluid">
 			{/* 介紹首頁 */}
 			<AboutHome />
+			{/* 自我介紹 */}
+			<AboutMe />
 		</main>
 	);
 }
---
+++ src/views/About_Me.js
@@ -0,0 +1,16 @@
+const AboutMe = () => {
+	return (
+		<div id="abMe" className="row h-100">
+			{/* 照片 */}
+			<div className="col-4">
+				
+			</div>
+			{/* 簡介 */}
+			<div className="col-8">
+
+			</div>
+		</div>
+	);
+}
+
+export default AboutMe;
