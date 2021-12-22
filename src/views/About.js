6a8a8a4ab5c1ed98918eb6920a8190ccf2d22e85
smoBEE <z94624@gmail.com>
2021-12-22 15:07:57 +0800

    About: 新增社群網站圖示。

--- src/views/About.js
+++ src/views/About.js
@@ -1,15 +1,11 @@
+import AboutHome from './About_Home';
 import './About.css';
 
 const About = () => {
 	return (
 		<main className="h-100 container-fluid">
-			<div id="abBanner" className="row h-100 d-flex flex-column align-items-center justify-content-center">
-				<div className="banner-text w-75">
-					<h1 className="text-white">我是黃健峯</h1>
-					<h3 className="text-white">I'm a (Your City) based (Your Occupation Here). Here will be your description. Use this to describe what you do or whatever you feel best describes yourself to a potential employer.</h3>
-					
-				</div>
-			</div>
+			{/* 介紹首頁 */}
+			<AboutHome />
 		</main>
 	);
 }
