a8f9ca54b237519bbc2210bf1f562e902fa8e6ea
smoBEE <z94624@gmail.com>
2022-01-06 11:32:14 +0800

    About: 1. 完成開發"名人區" 2. "聯絡區"開發中。

--- src/views/About_Email.js
+++ src/views/About_Email.js
@@ -1,11 +1,42 @@
 const AboutEmail = () => {
 	return (
-		<div id="abEmail" className="row h-100">
-			<div className="col col-8">
-				
+		<div id="abEmail" className="h-100 text-white p-5">
+			<div className="d-flex justify-content-start align-items-center">
+				<svg xmlns="http://www.w3.org/2000/svg" width="87" height="87" fill="currentColor" className="bi bi-envelope me-4" viewBox="0 0 16 16">
+					<path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+				</svg>
+				<h1 className="bold-900">Contact Me</h1>
 			</div>
-			<div className="col col-4">
+			<div className="row my-5">
+				<div className="col col-8">
+					<div className="mb-4 row">
+						<label htmlFor="abEmailName" className="col-sm-2 col-form-label">Name</label>
+						<div className="col-sm-10">
+							<input type="text" className="form-control form-control-lg" id="abEmailName" />
+						</div>
+					</div>
+					<div className="mb-4 row">
+						<label htmlFor="abEmailEmail" className="col-sm-2 col-form-label">Email</label>
+						<div className="col-sm-10">
+							<input type="email" className="form-control form-control-lg" id="abEmailEmail" />
+						</div>
+					</div>
+					<div className="mb-4 row">
+						<label htmlFor="abEmailSubject" className="col-sm-2 col-form-label">Subject</label>
+						<div className="col-sm-10">
+							<input type="text" className="form-control form-control-lg" id="abEmailSubject" />
+						</div>
+					</div>
+					<div className="mb-4 row">
+						<label htmlFor="abEmailMessage" className="col-sm-2 col-form-label">Message</label>
+						<div className="col-sm-10">
+							<textarea className="form-control form-control-lg" id="abEmailMessage" rows="5" />
+						</div>
+					</div>
+				</div>
+				<div className="col col-4">
 
+				</div>
 			</div>
 		</div>
 	);
--- src/views/About_Great.js
+++ src/views/About_Great.js
@@ -2,7 +2,9 @@ import Avatar from 'react-avatar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
 
-import einstein from './../images/Einstein.jpg';
+import einstein from './../images/AlbertEinstein.jpg';
+import hawking from './../images/StephenHawking.jpg';
+import jane from './../images/JaneGoodall.jpg';
 
 const AboutGreat = () => {
 	return (
@@ -13,9 +15,9 @@ const AboutGreat = () => {
 				<div className="col col-3 m-auto">
 					<Avatar
 						id="abGreatEinstein"
-						name="Einstein"
-						alt="Einstein"
-						title="Einstein"
+						name="Albert Einstein"
+						alt="Albert Einstein"
+						title="Albert Einstein"
 						size="150px" // 圖像大小
 						round={true} // border-radius；number, true(100%), false(0%)
 						src={einstein} // 載入照片
@@ -31,8 +33,66 @@ const AboutGreat = () => {
 							</p>
 						</blockquote>
 					</div>
-					<span className="fs-6 fst-italic align-self-end" >
+					<span className="col-5 fs-6 fst-italic align-self-end" >
 						- Albert Einstein
+					</span>
+				</div>
+			</div>
+			{/* 霍金 */}
+			<div id="abGreatHawkingSection" className="abGreatFigure row p-3">
+				{/* 名言 */}
+				<div className="col col-9 d-flex flex-column justify-content-center text-white">
+					<div className="col-10 align-self-center">
+						<blockquote className="blockquote text-start fs-4" >
+							<p>
+								<FontAwesomeIcon icon={faQuoteLeft} className="me-2" />
+								However difficult life may seem, there is always something you can do, and succeed at. It matters that you don’t just give up.
+							</p>
+						</blockquote>
+					</div>
+					<span className="col-5 fs-6 fst-italic align-self-end" >
+						- Stephen Hawking
+					</span>
+				</div>
+				{/* 頭像 */}
+				<div className="col col-3 m-auto">
+					<Avatar
+						id="abGreatHawking"
+						name="Stephen Hawking"
+						alt="Stephen Hawking"
+						title="Stephen Hawking"
+						size="150px" // 圖像大小
+						round={true} // border-radius；number, true(100%), false(0%)
+						src={hawking} // 載入照片
+					/>
+				</div>
+			</div>
+			{/* 珍古德 */}
+			<div id="abGreatJaneSection" className="abGreatFigure row p-3">
+				{/* 頭像 */}
+				<div className="col col-3 m-auto">
+					<Avatar
+						id="abGreatJane"
+						name="Jane Goodall"
+						alt="Jane Goodall"
+						title="Jane Goodall"
+						size="150px" // 圖像大小
+						round={true} // border-radius；number, true(100%), false(0%)
+						src={jane} // 載入照片
+					/>
+				</div>
+				{/* 名言 */}
+				<div className="col col-9 d-flex flex-column justify-content-center text-white">
+					<div className="col-10 align-self-center">
+						<blockquote className="blockquote text-start fs-4" >
+							<p>
+								<FontAwesomeIcon icon={faQuoteLeft} className="me-2" />
+								Only if we understand, can we care. Only if we care, we will help. Only if we help, we shall be saved.
+							</p>
+						</blockquote>
+					</div>
+					<span className="col-5 fs-6 fst-italic align-self-end" >
+						- Jane Goodall
 					</span>
 				</div>
 			</div>
