dfa9e41decff457f8c688823fa951e0f6f38ff62
smoBEE <z94624@gmail.com>
2022-01-25 22:06:10 +0800

    HitCounter: 處理今日人次在新的一天歸零。

--- src/views/HitCounter.js
+++ src/views/HitCounter.js
@@ -15,35 +15,45 @@ const checkUserCookie = (cookies) => {
     let dateRE = new RegExp(dateCookie.name, 'g');
     let dateExists = document.cookie.match(dateRE);
     let dateNow = new Date().getDate(); // 今天幾日
-    let isNewDate = true;
+    let newDateForCookie = true;
     if (!dateExists) { // Cookie 不存在
         createUserCookie(dateCookie, dateNow);
-        isNewDate = true;
+        newDateForCookie = true;
     } else { // Cookie 存在
         let dateLast = document.cookie
             .split('; ')
             .find(row => row.startsWith(dateCookie.name + '='))
             .split('=')[1]; // 上次登入日
-        isNewDate = dateNow != dateLast;
+        newDateForCookie = dateNow != dateLast;
         createUserCookie(dateCookie, dateNow);
     }
-    // 更新人次
-    cookies.map((cookie, cIdx) => {
-        let reqexp = new RegExp(cookie.name, 'g');
-        let exists = document.cookie.match(reqexp);
-        if (!exists) { // Cookie 不存在
-            createUserCookie(cookie, "True"); // 建立 Cookie
-            runTransaction(cookie.ref, (hits) => {
-                if (hits && (cIdx == 1 || isNewDate)) { // 已有資料庫 && (只要是累計 || 今天為新天)
-                    return (hits + 1);
-                } else if (!hits) { // 尚未建立資料庫
-                    return 1;
-                }
-            });
-        } else if (isNewDate) { // Cookie 存在 && 今天為新天
-            runTransaction(cookie.ref, (hits) => hits + 1);
-        }
+    // 判斷今日為新天且次數歸零
+    let newDateForFirebase = false;
+    runTransaction(ref(database, '/LastDate'), (date) => {
+        newDateForFirebase = dateNow != date;
+        return dateNow;
     });
+    // 更新人次
+    setTimeout(() => {
+        cookies.map((cookie, cIdx) => {
+            let reqexp = new RegExp(cookie.name, 'g');
+            let exists = document.cookie.match(reqexp);
+            if (!exists) { // Cookie 不存在
+                createUserCookie(cookie, "True"); // 建立 Cookie
+                runTransaction(cookie.ref, (hits) => {
+                    if (hits && (cIdx == 0 && newDateForFirebase)) { // 已有資料庫 && (今日 且 新天)
+                        return 1;
+                    } else if (hits && (cIdx == 1 || newDateForCookie)) { // 已有資料庫 && (只要是累計 || 今天為新天)
+                        return (hits + 1);
+                    } else if (!hits) { // 尚未建立資料庫
+                        return 1;
+                    }
+                });
+            } else if (newDateForCookie) { // Cookie 存在 && 今天為新天
+                runTransaction(cookie.ref, (hits) => hits + 1);
+            }
+        });
+    }, 1000);
 }
 // 建立 Cookie
 const createUserCookie = (cookie, value) => {
