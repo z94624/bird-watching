d4377c01fb0adbbaf3d5ae53014ebe2669911d33
smoBEE <z94624@gmail.com>
2022-01-27 08:57:03 +0800

    HitCounter: 處理 Firebase 非同步問題。

--- src/views/HitCounter.js
+++ src/views/HitCounter.js
@@ -9,28 +9,51 @@ import './HitCounter.css';
  * dateCookie: 日期 Cookie 資訊
  * hitCookies: 人次 Cookie 資訊
  */
-const updateHitCounter = (dateNow, dateCookie, hitCookies) => {
+const updateHitCounter = (dateNow, dateCookie, hitCookies, newDateForFirebase) => {
     // 當天新紀錄
     let newDateForCookie = checkNewDateForCookie(dateCookie, dateNow);
-    // 當天頭香
-    let newDateForFirebase = checkNewDateForFirebase(dateCookie, dateNow);
     // 更新人次
     hitCookies.forEach((cookie, cIdx) => {
+        let i = 0;
         let hitRE = new RegExp(cookie.name, 'g');
         let hitExists = document.cookie.match(hitRE);
         if (!hitExists) { // Cookie 不存在
-            createUserCookie(cookie, "True"); // 建立 Cookie
+            setTimeout(() => { // 避免存在性衝突
+                createUserCookie(cookie, "True"); // 建立 Cookie
+            }, 1000);
             runTransaction(cookie.ref, (hits) => {
                 if (hits && (cIdx === 0 && newDateForFirebase)) { // 已建資料 && (今日人次 && 當天頭香)
-                    return 1;
+                    i += 1;
+                    if (i === 1) { // 只允許跑一次
+                        return 1;
+                    } else {
+                        return hits;
+                    }
                 } else if (hits && (cIdx === 1 || newDateForCookie)) { // 已建資料 && (只要是累計人次 || 當天新紀錄)
-                    return (hits + 1);
+                    i += 1;
+                    if (i === 1) { // 只允許跑一次
+                        return (hits + 1);
+                    } else {
+                        return hits;
+                    }
                 } else if (!hits) { // 未建資料
-                    return 1;
+                    i += 1;
+                    if (i === 1) { // 只允許跑一次
+                        return 1;
+                    } else {
+                        return hits;
+                    }
                 }
             });
         } else if (newDateForCookie) { // Cookie 存在 && 當天新紀錄
-            runTransaction(cookie.ref, (hits) => hits + 1);
+            runTransaction(cookie.ref, (hits) => {
+                i += 1;
+                if (i === 1) { // 只允許跑一次
+                    return (hits + 1);
+                } else {
+                    return hits;
+                }
+            });
         }
     });
 }
@@ -48,18 +71,27 @@ const checkNewDateForCookie = (dateCookie, dateNow) => {
             .find(row => row.startsWith(dateCookie.name + '='))
             .split('=')[1]); // 上次登入日
         newDateForCookie = dateNow !== dateLast;
-        createUserCookie(dateCookie, dateNow);
+        setTimeout(() => { // 避免存在性衝突
+            createUserCookie(dateCookie, dateNow);
+        }, 1000);
     }
     return newDateForCookie;
 }
 // 判斷當天首位拜訪者，今日人次歸零
-const checkNewDateForFirebase = (dateCookie, dateNow) => {
+const checkNewDateForFirebase = (dateNow, dateCookie, hitCookies) => {
+    //createUserCookie(dateCookie, 20);
+    let i = 0;
     let newDateForFirebase = false;
     runTransaction(dateCookie.ref, (date) => {
-        newDateForFirebase = dateNow !== date;
+        if (date) { // 有取得資料
+            i += 1;
+            if (i === 1) { // 更新前取得資料
+                newDateForFirebase = dateNow !== date;
+                updateHitCounter(dateNow, dateCookie, hitCookies, newDateForFirebase);
+            }
+        }
         return dateNow;
     });
-    return newDateForFirebase;
 }
 // 建立 Cookie
 const createUserCookie = (cookie, value) => {
@@ -80,7 +112,7 @@ const HitCounter = () => {
         {name: "TodayHits", expire: new Date(yearNow, monthNow, dateNow+1, 0, 0, 0).toUTCString(), ref: ref(database, '/TodayHits'), id: "todayHitCounter", nameChi: "今日人次"},
         {name: "TotalHits", expire: infiniteExpiryDate.toUTCString(), ref: ref(database, '/TotalHits'), id: "totalHitCounter", nameChi: "累計人次"}
     ];
-    updateHitCounter(dateNow, dateCookie, hitCookies); // 更新人次
+    checkNewDateForFirebase(dateNow, dateCookie, hitCookies); // 更新人次
     // 最新人次
     const newHits = hitCookies.reduce((obj, cookie) => {
         onValue(cookie.ref, (snapshot) => {
