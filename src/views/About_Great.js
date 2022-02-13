29892052312a802ed99460c932c12c570693e7c2
smoBEE <z94624@gmail.com>
2022-02-13 12:45:45 +0800

    About: 新增"西雅圖酋長"(給總統的一封信)。

--- src/views/About_Great.js
+++ src/views/About_Great.js
@@ -5,10 +5,12 @@ import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
 import einstein from './../images/AlbertEinstein.jpg';
 import hawking from './../images/StephenHawking.jpg';
 import jane from './../images/JaneGoodall.jpg';
+import sese from './../images/ChiefSeattle.jpg';
 // 偉人
 const greats = [
 	{name: "Albert Einstein", briefName: "Einstein", src: einstein, quote: "There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle."}, // 愛因斯坦
 	{name: "Stephen Hawking", briefName: "Hawking", src: hawking, quote: "However difficult life may seem, there is always something you can do, and succeed at. It matters that you don’t just give up."}, // 霍金
+	{name: "Chief Seattle", briefName: "SeSe", src: sese, quote: "All things share the same breath - the beast, the tree, the man. The air shares its spirit with all the life it supports."}, // 西雅圖酋長
 	{name: "Jane Goodall", briefName: "Jane", src: jane, quote: "Only if we understand, can we care. Only if we care, we will help. Only if we help, we shall be saved."} // 珍古德
 ];
 
