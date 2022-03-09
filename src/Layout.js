3a0be556f66d7cdf6332c02b9a2323074dd8a179
smoBEE <z94624@gmail.com>
2022-03-09 10:34:55 +0800

    BgMusic: 設計成拉拉(黃化玄鳳)頭。

--- src/Layout.js
+++ src/Layout.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 
 import './Layout.css';
 
-import birdsSinging from './images/birdsSinging.png';
+import cockatielCrest from './images/cockatielCrest.png';
 
 const Layout = ({children}) => {
 	return (
@@ -14,7 +14,7 @@ const Layout = ({children}) => {
 					<Header className="fixed-top" /> {/* 每個分頁都要有途徑轉換至其他分頁 */}
 					{children} {/* 每個分頁的頁面 */}
 					<div id="backgroundPlaylist"> {/* 背景音樂 */}
-						<div id="bpImg"><img src={birdsSinging} alt="" /></div>
+						<div id="bpImg"><img src={cockatielCrest} alt="" /></div>
 						<iframe src="https://www.youtube.com/embed/videoseries?list=PLTks92J980S2_HbgX6drngE3T41uiCgyc&autoplay=1&disablekb=1&loop=1&modestbranding=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 					</div>
 				</div>
