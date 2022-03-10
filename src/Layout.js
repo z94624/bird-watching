83aff1ae637c7bda87ef4f5d35e23e950130ac5c
smoBEE <z94624@gmail.com>
2022-03-10 22:58:47 +0800

    BgMusic: 繼續設計。

--- src/Layout.js
+++ src/Layout.js
@@ -1,4 +1,6 @@
-import { Fragment } from 'react';
+import { Fragment, useRef } from 'react';
+
+import Draggable from 'react-draggable';
 
 import Header from './components/Header';
 
@@ -7,16 +9,25 @@ import './Layout.css';
 import cockatielCrest from './images/cockatielCrest.png';
 
 const Layout = ({children}) => {
+	const nodeRef = useRef(null); // react-draggable: If running in React Strict mode, ReactDOM.findDOMNode() is deprecated.
+
 	return (
 		<Fragment>
 			<div className="h-100 bg-dark text-center">
             	<div className="cover-container d-flex h-100 p-0 flex-column">
 					<Header className="fixed-top" /> {/* 每個分頁都要有途徑轉換至其他分頁 */}
 					{children} {/* 每個分頁的頁面 */}
-					<div id="backgroundPlaylist"> {/* 背景音樂 */}
-						<div id="bpImg"><img src={cockatielCrest} alt="" /></div>
-						<iframe src="https://www.youtube.com/embed/videoseries?list=PLTks92J980S2_HbgX6drngE3T41uiCgyc&autoplay=1&disablekb=1&loop=1&modestbranding=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-					</div>
+
+					<Draggable
+						bounds="parent"
+						defaultPosition={{x: 0, y: 200}}
+						nodeRef={nodeRef}
+					>
+						<div id="backgroundPlaylist" ref={nodeRef}> {/* 背景音樂 */}
+							<div id="bpImg"><img src={cockatielCrest} alt="" /></div>
+							<iframe src="https://www.youtube.com/embed/videoseries?list=PLTks92J980S2_HbgX6drngE3T41uiCgyc&autoplay=1&disablekb=1&loop=1&modestbranding=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+						</div>
+					</Draggable>
 				</div>
         	</div>
 		</Fragment>
