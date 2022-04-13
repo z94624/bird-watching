849b182f04d88737d618dd98bbf289715502411c
smoBEE <z94624@gmail.com>
2022-04-13 22:49:25 +0800

    Identity: 建立蝴蝶資料集。

---
+++ src/utils/identity-butterflies.js
@@ -0,0 +1,76 @@
+import pseudozizeeriaMaha_ms from './../images/identity/butterfly/藍灰蝶♂.jpg';
+import zizinaOtis_ms from './../images/identity/butterfly/折列藍灰蝶♂.jpg';
+import zizulaHylax_ms from './../images/identity/butterfly/迷你藍灰蝶♂.jpg';
+import zizeeriaKarsandra_xs from './../images/identity/butterfly/莧藍灰蝶.jpg';
+
+import euploeaTulliolus_xs from './../images/identity/butterfly/小紫斑蝶.jpg';
+import euploeaEunice_fs from './../images/identity/butterfly/圓翅紫斑蝶♀.jpg';
+import euploeaSylvester_xs from './../images/identity/butterfly/雙標紫斑蝶.jpg';
+import euploeaMulciber_ms from './../images/identity/butterfly/異紋紫斑蝶♂.jpg';
+// 蝴蝶基本資料
+export const infos = [
+	{
+		"name_chi": "藍灰蝶", // 中文名
+		"name_latin": "Zizeeria maha okinawana", // 拉丁名
+		"sex": "♂", // 性別
+		"direction": "s", // 觀察方向：head=h, tail=t, side=s
+		"href": "http://dearlep.tw/species.html?namecode=345798&photo_id=7d56bda86cd86e72ca85921d2b19e47c", // Source Path
+		"butterfly": pseudozizeeriaMaha_ms // 在地照片
+	},
+	{
+		"name_chi": "折列藍灰蝶",
+		"name_latin": "Zizina otis riukuensis",
+		"sex": "♂",
+		"direction": "s",
+		"href": "http://dearlep.tw/species.html?namecode=345800&photo_id=fea50bc592f736e3530c49eeceba3e55",
+		"butterfly": zizinaOtis_ms
+	},
+	{
+		"name_chi": "迷你藍灰蝶",
+		"name_latin": "Zizula hylax",
+		"sex": "♂",
+		"direction": "s",
+		"href": "http://dearlep.tw/species.html?namecode=345801&photo_id=7a2ef68fff5a26727c31b8e4bb8a1f29",
+		"butterfly": zizulaHylax_ms
+	},
+	{
+		"name_chi": "莧藍灰蝶",
+		"name_latin": "Zizeeria karsandra",
+		"sex": "",
+		"direction": "s",
+		"href": "http://dearlep.tw/species.html?namecode=345797&photo_id=0850d25e9d2d0e037f81cb7a1c44bf9e",
+		"butterfly": zizeeriaKarsandra_xs
+	},
+	{
+		"name_chi": "小紫斑蝶",
+		"name_latin": "Euploea tulliolus koxinga Fruhstorfer",
+		"sex": "",
+		"direction": "s",
+		"href": "http://dearlep.tw/species.html?namecode=347106&photo_id=467d9cae07073f30d4420392182f0f6f",
+		"butterfly": euploeaTulliolus_xs
+	},
+	{
+		"name_chi": "圓翅紫斑蝶",
+		"name_latin": "Euploea eunice hobsoni",
+		"sex": "♀",
+		"direction": "s",
+		"href": "http://dearlep.tw/species.html?namecode=347099&photo_id=ada57cc8be8d65a4ad0f9bd7cf33da70",
+		"butterfly": euploeaEunice_fs
+	},
+	{
+		"name_chi": "雙標紫斑蝶",
+		"name_latin": "Euploea sylvester swinhoei",
+		"sex": "",
+		"direction": "s",
+		"href": "http://dearlep.tw/species.html?namecode=347105&photo_id=f255e912cd73d04ab1d4484f19079458",
+		"butterfly": euploeaSylvester_xs
+	},
+	{
+		"name_chi": "異紋紫斑蝶",
+		"name_latin": "Euploea mulciber barsine",
+		"sex": "♂",
+		"direction": "s",
+		"href": "http://dearlep.tw/species.html?namecode=347102&photo_id=0c6fb8b9c1d7b810de7b557038f9eb51",
+		"butterfly": euploeaMulciber_ms
+	}
+];
