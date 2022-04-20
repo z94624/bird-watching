f9761c97d1f4237b7659719a0fdcec51788ae218
smoBEE <z94624@gmail.com>
2022-04-20 20:43:21 +0800

    Identity: 調整卡牌 RWD 大小。

--- src/utils/identity-butterflies.js
+++ src/utils/identity-butterflies.js
@@ -1,19 +1,20 @@
-import pseudozizeeriaMaha_ms from './../images/identity/butterfly/藍灰蝶♂.jpg';
-import zizinaOtis_ms from './../images/identity/butterfly/折列藍灰蝶♂.jpg';
-import zizulaHylax_ms from './../images/identity/butterfly/迷你藍灰蝶♂.jpg';
-import zizeeriaKarsandra_xs from './../images/identity/butterfly/莧藍灰蝶.jpg';
+import pseudozizeeriaMaha_ms from './../images/identity/butterfly/藍灰蝶1.jpg';
+import zizinaOtis_ms from './../images/identity/butterfly/折列藍灰蝶1.jpg';
+import zizulaHylax_ms from './../images/identity/butterfly/迷你藍灰蝶1.jpg';
+import zizeeriaKarsandra_xs from './../images/identity/butterfly/莧藍灰蝶1.jpg';
 
-import euploeaTulliolus_xs from './../images/identity/butterfly/小紫斑蝶.jpg';
-import euploeaEunice_fs from './../images/identity/butterfly/圓翅紫斑蝶♀.jpg';
-import euploeaSylvester_xs from './../images/identity/butterfly/雙標紫斑蝶.jpg';
-import euploeaMulciber_ms from './../images/identity/butterfly/異紋紫斑蝶♂.jpg';
+import euploeaTulliolus_xs from './../images/identity/butterfly/小紫斑蝶1.jpg';
+import euploeaEunice_fs from './../images/identity/butterfly/圓翅紫斑蝶1.jpg';
+import euploeaSylvester_xs from './../images/identity/butterfly/雙標紫斑蝶1.jpg';
+import euploeaMulciber_ms from './../images/identity/butterfly/異紋紫斑蝶1.jpg';
 // 蝴蝶基本資料
 export const butterflyInfos = [
 	{
 		"name_chi": "藍灰蝶", // 中文名
 		"name_latin": "Zizeeria maha okinawana", // 拉丁名
 		"sex": "♂", // 性別
-		"direction": "s", // 觀察方向：head=h, tail=t, side=s
+		"direction": "s",
+		"feature": "藍灰蝶後翅中央的小斑紋排成環形，折列藍灰蝶則因為從前往後數來的第二個斑紋向內偏移而使這些小斑排成兩列；莧藍灰蝶的翅背面藍色的鱗片顏色較深，而且前翅中央的斑列顏色特別深。", // 觀察方向：head=h, tail=t, side=s
 		"href": "http://dearlep.tw/species.html?namecode=345798&photo_id=7d56bda86cd86e72ca85921d2b19e47c", // Source Path
 		"butterfly": pseudozizeeriaMaha_ms // 在地照片
 	},
@@ -22,6 +23,7 @@ export const butterflyInfos = [
 		"name_latin": "Zizina otis riukuensis",
 		"sex": "♂",
 		"direction": "s",
+		"feature": "後翅中央的斑列由前向後數來的第二個斑折向內側。",
 		"href": "http://dearlep.tw/species.html?namecode=345800&photo_id=fea50bc592f736e3530c49eeceba3e55",
 		"butterfly": zizinaOtis_ms
 	},
@@ -30,6 +32,7 @@ export const butterflyInfos = [
 		"name_latin": "Zizula hylax",
 		"sex": "♂",
 		"direction": "s",
+		"feature": "前翅腹面靠近前緣處多了兩個小黑點，而前後翅外緣內側各有一條細線紋。",
 		"href": "http://dearlep.tw/species.html?namecode=345801&photo_id=7a2ef68fff5a26727c31b8e4bb8a1f29",
 		"butterfly": zizulaHylax_ms
 	},
@@ -38,6 +41,7 @@ export const butterflyInfos = [
 		"name_latin": "Zizeeria karsandra",
 		"sex": "",
 		"direction": "s",
+		"feature": "翅上的藍色鱗色調較暗，而且前翅腹面中央的弧形斑列顏色比外緣斑列色彩要來得暗，在藍灰蝶則色調相同。",
 		"href": "http://dearlep.tw/species.html?namecode=345797&photo_id=0850d25e9d2d0e037f81cb7a1c44bf9e",
 		"butterfly": zizeeriaKarsandra_xs
 	},
@@ -46,6 +50,7 @@ export const butterflyInfos = [
 		"name_latin": "Euploea tulliolus koxinga Fruhstorfer",
 		"sex": "",
 		"direction": "s",
+		"feature": "前翅腹面中央有一枚白色斑點，體型是台灣產紫斑蝶屬中最小，翅形近似圓翅紫斑蝶，雄蝶前後緣呈弧形突出，後翅背面中室附近有一片灰褐色性斑；雌蝶前翅後緣則呈一直線。",
 		"href": "http://dearlep.tw/species.html?namecode=347106&photo_id=467d9cae07073f30d4420392182f0f6f",
 		"butterfly": euploeaTulliolus_xs
 	},
@@ -54,6 +59,7 @@ export const butterflyInfos = [
 		"name_latin": "Euploea eunice hobsoni",
 		"sex": "♀",
 		"direction": "s",
+		"feature": "前翅背側中央的淺色紋，其他種類在相同的位置沒有類似的斑紋。",
 		"href": "http://dearlep.tw/species.html?namecode=347099&photo_id=ada57cc8be8d65a4ad0f9bd7cf33da70",
 		"butterfly": euploeaEunice_fs
 	},
@@ -62,6 +68,7 @@ export const butterflyInfos = [
 		"name_latin": "Euploea sylvester swinhoei",
 		"sex": "",
 		"direction": "s",
+		"feature": "雄蝶前翅後緣內側有二條黑色平行性標，雄蝶前翅後緣弧形突出不明顯；雌蝶前翅後緣則呈一直線。",
 		"href": "http://dearlep.tw/species.html?namecode=347105&photo_id=f255e912cd73d04ab1d4484f19079458",
 		"butterfly": euploeaSylvester_xs
 	},
@@ -70,6 +77,7 @@ export const butterflyInfos = [
 		"name_latin": "Euploea mulciber barsine",
 		"sex": "♂",
 		"direction": "s",
+		"feature": "前翅背面端部有紫色金屬光澤。雄蝶後翅前緣有一大片性標，前翅後緣弧形突出不明顯；雌翅前翅後緣弧形突出不明顯；雌蝶則翅後緣則呈一直線，後翅則有放射狀白條紋，排列方式近似小紋青斑蝶。",
 		"href": "http://dearlep.tw/species.html?namecode=347102&photo_id=0c6fb8b9c1d7b810de7b557038f9eb51",
 		"butterfly": euploeaMulciber_ms
 	}
