d5da541e7325ee928b977b6d186e984e53e86a8e
smoBEE <z94624@gmail.com>
2022-07-11 22:12:13 +0800

    Identity: 新增多種蛺蝶。

--- src/utils/identity/butterfly/nymphalidae.js
+++ src/utils/identity/butterfly/nymphalidae.js
@@ -1,36 +1,127 @@
 // 蛺蝶科
-import euploeaTulliolus_xs from './../../../images/identity/butterfly/小紫斑蝶1.jpg';
-import euploeaEunice_fs from './../../../images/identity/butterfly/圓翅紫斑蝶1.jpg';
-import euploeaSylvester_xs from './../../../images/identity/butterfly/雙標紫斑蝶1.jpg';
-import euploeaMulciber_ms from './../../../images/identity/butterfly/異紋紫斑蝶1.jpg';
-
-import paranticaAglea_ms from './../../../images/identity/butterfly/絹斑蝶1.jpg';
-import ideopsisSimilis_xs from './../../../images/identity/butterfly/旖斑蝶1.jpg';
+import libytheaLepita_fs from './../../../images/identity/butterfly/東方喙蝶1.jpg';
+import danausGenutia_ms from './../../../images/identity/butterfly/虎斑蝶1.jpg';
+import danausChrysippus_xs from './../../../images/identity/butterfly/金斑蝶1.jpg';
 import tirumalaLimniace_xs from './../../../images/identity/butterfly/淡紋青斑蝶1.jpg';
 import tirumalaSeptentrionis_xs from './../../../images/identity/butterfly/小紋青斑蝶1.jpg';
+import paranticaAglea_ms from './../../../images/identity/butterfly/絹斑蝶1.jpg';
 import paranticaSwinhoei_fs from './../../../images/identity/butterfly/斯氏絹斑蝶1.jpg';
 import paranticaSita_xs from './../../../images/identity/butterfly/大絹斑蝶1.jpg';
+import ideopsisSimilis_xs from './../../../images/identity/butterfly/旖斑蝶1.jpg';
+import euploeaSylvester_xs from './../../../images/identity/butterfly/雙標紫斑蝶1.jpg';
+import euploeaMulciber_ms from './../../../images/identity/butterfly/異紋紫斑蝶1.jpg';
+import euploeaEunice_fs from './../../../images/identity/butterfly/圓翅紫斑蝶1.jpg';
+import euploeaTulliolus_xs from './../../../images/identity/butterfly/小紫斑蝶1.jpg';
+import ideaLeuconoe_xh from './../../../images/identity/butterfly/大白斑蝶1.jpg';
+import acraeaIssoria_mh from './../../../images/identity/butterfly/苧麻珍蝶1.jpg';
+import argyreusHyperbius_ms from './../../../images/identity/butterfly/斐豹蛺蝶1.jpg';
+import phalantaPhalantha_xh from './../../../images/identity/butterfly/琺蛺蝶1.jpg';
+import cuphaErymanthis_mh from './../../../images/identity/butterfly/黃襟蛺蝶1.jpg';
+import junoniaIphita_xh from './../../../images/identity/butterfly/黯眼蛺蝶1.jpg';
+import junoniaAlmana_xh from './../../../images/identity/butterfly/眼蛺蝶1.jpg';
+import junoniaLemonias_xh from './../../../images/identity/butterfly/鱗紋眼蛺蝶1.jpg';
+import junoniaOrithya_xh from './../../../images/identity/butterfly/青眼蛺蝶1.jpg';
+import kallimaInachus_xs from './../../../images/identity/butterfly/枯葉蝶1.jpg';
+import yomaSabina_xh from './../../../images/identity/butterfly/黃帶隱蛺蝶1.jpg';
+import vanessaIndica_xh from './../../../images/identity/butterfly/大紅蛺蝶1.jpg';
 
 export const nymphalidae = [
-    {
-		"name_chi": "小紫斑蝶",
-		"name_latin": "Euploea tulliolus koxinga Fruhstorfer",
-		"sex": "",
+	{
+		"name_chi": "東方喙蝶",
+		"name_latin": "Libythea lepita formosana Fruhstorfer",
+		"sex": "f",
 		"direction": "s",
-		"feature": "前翅腹面中央有一枚白色斑點，體型是台灣產紫斑蝶屬中最小，翅形近似圓翅紫斑蝶，雄蝶前後緣呈弧形突出，後翅背面中室附近有一片灰褐色性斑；雌蝶前翅後緣則呈一直線。",
-		"href": "http://dearlep.tw/species.html?namecode=347106&photo_id=467d9cae07073f30d4420392182f0f6f",
-		"image": euploeaTulliolus_xs,
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347145&photo_id=fa43e894201f841b7247eb95cded4d20",
+		"image": libytheaLepita_fs,
 		"family_chi": "蛺蝶科",
 		"family_latin": "Nymphalidae"
 	},
 	{
-		"name_chi": "圓翅紫斑蝶",
-		"name_latin": "Euploea eunice hobsoni",
+		"name_chi": "虎斑蝶",
+		"name_latin": "Danaus genutia",
+		"sex": "m",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347089&photo_id=79cb359f62832d77d76d2ba744a6bc1e",
+		"image": danausGenutia_ms,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "金斑蝶",
+		"name_latin": "Danaus chrysippus",
+		"sex": "",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347088&photo_id=5709daecb9df9c90e47cf26337d7f4a5",
+		"image": danausChrysippus_xs,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+    {
+		"name_chi": "淡紋青斑蝶",
+		"name_latin": "Tirumala limniace limniace",
+		"sex": "",
+		"direction": "s",
+		"feature": "沒有細肩帶，但是有粗高跟鞋。",
+		"href": "http://dearlep.tw/species.html?namecode=347203&photo_id=2af48e99028673b03bf6f26d17eeb818",
+		"image": tirumalaLimniace_xs,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "小紋青斑蝶",
+		"name_latin": "Tirumala septentrionis",
+		"sex": "",
+		"direction": "s",
+		"feature": "沒有細肩帶，但是有細高跟鞋。",
+		"href": "http://dearlep.tw/species.html?namecode=347205&photo_id=b19b011890f9e6089cceac8ec48dc226",
+		"image": tirumalaSeptentrionis_xs,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "絹斑蝶",
+		"name_latin": "Parantica aglea maghaba",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "有細肩帶，而且條紋斑點偏白。",
+		"href": "http://dearlep.tw/species.html?namecode=347183&photo_id=2172ebed8c42de274f284284c94d574c",
+		"image": paranticaAglea_ms,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "斯氏絹斑蝶",
+		"name_latin": "Parantica swinhoei",
 		"sex": "♀",
 		"direction": "s",
-		"feature": "前翅背側中央的淺色紋，其他種類在相同的位置沒有類似的斑紋。",
-		"href": "http://dearlep.tw/species.html?namecode=347099&photo_id=ada57cc8be8d65a4ad0f9bd7cf33da70",
-		"image": euploeaEunice_fs,
+		"feature": "沒有細肩帶也沒有高跟鞋，但是穿紅禮服黑褐裙襬；看不到裙擺，就看合翅時，後翅斑點無內凹的缺口。",
+		"href": "http://dearlep.tw/species.html?namecode=347186&photo_id=9e4ca5335bffb56e809d951723323921",
+		"image": paranticaSwinhoei_fs,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "大絹斑蝶",
+		"name_latin": "Parantica sita niphonica",
+		"sex": "",
+		"direction": "s",
+		"feature": "沒有細肩帶也沒有高跟鞋，但是穿黑褐禮服紅裙襬；看不到裙擺，就看合翅時，後翅斑點有內凹的缺口。",
+		"href": "http://dearlep.tw/species.html?namecode=347185&photo_id=b7b42f66db33b1b33d304ca1a03ce947",
+		"image": paranticaSita_xs,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "旖斑蝶",
+		"name_latin": "Ideopsis similis",
+		"sex": "",
+		"direction": "s",
+		"feature": "有細肩帶，而且條紋斑點偏藍。",
+		"href": "http://dearlep.tw/species.html?namecode=347124&photo_id=77e74970e1c5a25e5fc284d8f27497a4",
+		"image": ideopsisSimilis_xs,
 		"family_chi": "蛺蝶科",
 		"family_latin": "Nymphalidae"
 	},
@@ -57,68 +148,156 @@ export const nymphalidae = [
 		"family_latin": "Nymphalidae"
 	},
 	{
-		"name_chi": "絹斑蝶",
-		"name_latin": "Parantica aglea maghaba",
-		"sex": "♂",
-		"direction": "s",
-		"feature": "有細肩帶，而且條紋斑點偏白。",
-		"href": "http://dearlep.tw/species.html?namecode=347183&photo_id=2172ebed8c42de274f284284c94d574c",
-		"image": paranticaAglea_ms,
-		"family_chi": "蛺蝶科",
-		"family_latin": "Nymphalidae"
-	},
-	{
-		"name_chi": "旖斑蝶",
-		"name_latin": "Ideopsis similis",
-		"sex": "",
-		"direction": "s",
-		"feature": "有細肩帶，而且條紋斑點偏藍。",
-		"href": "http://dearlep.tw/species.html?namecode=347124&photo_id=77e74970e1c5a25e5fc284d8f27497a4",
-		"image": ideopsisSimilis_xs,
-		"family_chi": "蛺蝶科",
-		"family_latin": "Nymphalidae"
-	},
-	{
-		"name_chi": "淡紋青斑蝶",
-		"name_latin": "Tirumala limniace limniace",
-		"sex": "",
-		"direction": "s",
-		"feature": "沒有細肩帶，但是有粗高跟鞋。",
-		"href": "http://dearlep.tw/species.html?namecode=347203&photo_id=2af48e99028673b03bf6f26d17eeb818",
-		"image": tirumalaLimniace_xs,
-		"family_chi": "蛺蝶科",
-		"family_latin": "Nymphalidae"
-	},
-	{
-		"name_chi": "小紋青斑蝶",
-		"name_latin": "Tirumala septentrionis",
-		"sex": "",
-		"direction": "s",
-		"feature": "沒有細肩帶，但是有細高跟鞋。",
-		"href": "http://dearlep.tw/species.html?namecode=347205&photo_id=b19b011890f9e6089cceac8ec48dc226",
-		"image": tirumalaSeptentrionis_xs,
-		"family_chi": "蛺蝶科",
-		"family_latin": "Nymphalidae"
-	},
-	{
-		"name_chi": "斯氏絹斑蝶",
-		"name_latin": "Parantica swinhoei",
+		"name_chi": "圓翅紫斑蝶",
+		"name_latin": "Euploea eunice hobsoni",
 		"sex": "♀",
 		"direction": "s",
-		"feature": "沒有細肩帶也沒有高跟鞋，但是穿紅禮服黑褐裙襬；看不到裙擺，就看合翅時，後翅斑點無內凹的缺口。",
-		"href": "http://dearlep.tw/species.html?namecode=347186&photo_id=9e4ca5335bffb56e809d951723323921",
-		"image": paranticaSwinhoei_fs,
+		"feature": "前翅背側中央的淺色紋，其他種類在相同的位置沒有類似的斑紋。",
+		"href": "http://dearlep.tw/species.html?namecode=347099&photo_id=ada57cc8be8d65a4ad0f9bd7cf33da70",
+		"image": euploeaEunice_fs,
 		"family_chi": "蛺蝶科",
 		"family_latin": "Nymphalidae"
 	},
 	{
-		"name_chi": "大絹斑蝶",
-		"name_latin": "Parantica sita niphonica",
+		"name_chi": "小紫斑蝶",
+		"name_latin": "Euploea tulliolus koxinga Fruhstorfer",
 		"sex": "",
 		"direction": "s",
-		"feature": "沒有細肩帶也沒有高跟鞋，但是穿黑褐禮服紅裙襬；看不到裙擺，就看合翅時，後翅斑點有內凹的缺口。",
-		"href": "http://dearlep.tw/species.html?namecode=347185&photo_id=b7b42f66db33b1b33d304ca1a03ce947",
-		"image": paranticaSita_xs,
+		"feature": "前翅腹面中央有一枚白色斑點，體型是台灣產紫斑蝶屬中最小，翅形近似圓翅紫斑蝶，雄蝶前後緣呈弧形突出，後翅背面中室附近有一片灰褐色性斑；雌蝶前翅後緣則呈一直線。",
+		"href": "http://dearlep.tw/species.html?namecode=347106&photo_id=467d9cae07073f30d4420392182f0f6f",
+		"image": euploeaTulliolus_xs,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "大白斑蝶",
+		"name_latin": "Idea leuconoe clara",
+		"sex": "",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347122&photo_id=a84c0eac5c205f12e8703cc719c01955",
+		"image": ideaLeuconoe_xh,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "苧麻珍蝶",
+		"name_latin": "Acraea issoria formosana",
+		"sex": "m",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347069&photo_id=b5c4610171c88dfde65ed56b25bd0f4e",
+		"image": acraeaIssoria_mh,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "斐豹蛺蝶",
+		"name_latin": "Argyreus hyperbius",
+		"sex": "m",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347073&photo_id=92da8f42ce9bc3d43afcb35e9ce9af48",
+		"image": argyreusHyperbius_ms,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "琺蛺蝶",
+		"name_latin": "Phalanta phalantha",
+		"sex": "",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347190&photo_id=bd51a2ae9685bc11916354f3aa471e91",
+		"image": phalantaPhalantha_xh,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "黃襟蛺蝶",
+		"name_latin": "Cupha erymanthis",
+		"sex": "m",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347086&photo_id=17d401dba78bb5466325d7fcebf5adae",
+		"image": cuphaErymanthis_mh,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "黯眼蛺蝶",
+		"name_latin": "Junonia iphita",
+		"sex": "",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347128&photo_id=a241e3eb1041ba1ba95efdef30181fd1",
+		"image": junoniaIphita_xh,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "眼蛺蝶",
+		"name_latin": "Junonia almana",
+		"sex": "",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347125&photo_id=cf96c8221f76523d37a92e1f1c6b5724",
+		"image": junoniaAlmana_xh,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "鱗紋眼蛺蝶",
+		"name_latin": "Junonia lemonias aenaria",
+		"sex": "",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347129&photo_id=8730730a46018e9f7002e5043a08db36",
+		"image": junoniaLemonias_xh,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "青眼蛺蝶",
+		"name_latin": "Junonia orithya",
+		"sex": "",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347130&photo_id=82e7bc8a2d1434e31b5a6366a5967efa",
+		"image": junoniaOrithya_xh,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "枯葉蝶",
+		"name_latin": "Kallima inachus formosana Fruhstorfer",
+		"sex": "",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347131&photo_id=d5c0377232cc7a0bbe6701035e86f641",
+		"image": kallimaInachus_xs,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "黃帶隱蛺蝶",
+		"name_latin": "Yoma sabina podium Tsukada",
+		"sex": "",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347209&photo_id=85690a5cb865846f22b03e7b49e34a84",
+		"image": yomaSabina_xh,
+		"family_chi": "蛺蝶科",
+		"family_latin": "Nymphalidae"
+	},
+	{
+		"name_chi": "大紅蛺蝶",
+		"name_latin": "Vanessa indica",
+		"sex": "",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347207&photo_id=e2caf35779ecbd94e741e81b50edd684",
+		"image": vanessaIndica_xh,
 		"family_chi": "蛺蝶科",
 		"family_latin": "Nymphalidae"
 	}
