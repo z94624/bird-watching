cfb8a277b43481d15f6a9583e7ee079b9da2d8fd
smoBEE <z94624@gmail.com>
2022-07-19 09:26:37 +0800

    Identity: 新增必考灰蝶。

--- src/utils/identity/butterfly/lycaenidae.js
+++ src/utils/identity/butterfly/lycaenidae.js
@@ -1,32 +1,231 @@
 // 灰蝶科
+import curetisAcuta_xs from './../../../images/identity/butterfly/銀灰蝶1.jpg';
+import heliophorusIla_xs from './../../../images/identity/butterfly/紫日灰蝶1.jpg';
+import arhopalaBazalus_xs from './../../../images/identity/butterfly/燕尾紫灰蝶1.jpg';
+import arhopalaJaponica_xs from './../../../images/identity/butterfly/日本紫灰蝶1.jpg';
+import mahathalaAmeria_ms from './../../../images/identity/butterfly/凹翅紫灰蝶1.jpg';
+import artipeEryx_xs from './../../../images/identity/butterfly/綠灰蝶1.jpg';
+import rapalaVaruna_xs from './../../../images/identity/butterfly/燕灰蝶1.jpg';
+import spindasisSyama_fs from './../../../images/identity/butterfly/三斑虎灰蝶1.jpg';
+import spindasisLohita_fs from './../../../images/identity/butterfly/虎灰蝶1.jpg';
+import nacadubaKurava_ms from './../../../images/identity/butterfly/大娜波灰蝶1.jpg';
+import prosotasNora_ms from './../../../images/identity/butterfly/波灰蝶1.jpg';
+import jamidesBochus_mfs from './../../../images/identity/butterfly/雅波灰蝶1.jpg';
+import jamidesAlecto_fs from './../../../images/identity/butterfly/淡青雅波灰蝶1.jpg';
+import pithecopsFulgens_xs from './../../../images/identity/butterfly/藍丸灰蝶1.jpg';
+import neopithecopsZalmora_fs from './../../../images/identity/butterfly/黑點灰蝶1.jpg';
+import megisbaMalaya_fs from './../../../images/identity/butterfly/黑星灰蝶1.jpg';
+import acytolepisPuspa_ms from './../../../images/identity/butterfly/靛色琉灰蝶1.jpg';
+
 import pseudozizeeriaMaha_ms from './../../../images/identity/butterfly/藍灰蝶1.jpg';
 import zizinaOtis_ms from './../../../images/identity/butterfly/折列藍灰蝶1.jpg';
 import zizulaHylax_ms from './../../../images/identity/butterfly/迷你藍灰蝶1.jpg';
 import zizeeriaKarsandra_xs from './../../../images/identity/butterfly/莧藍灰蝶1.jpg';
 
-import nacadubaKurava_ms from './../../../images/identity/butterfly/大娜波灰蝶1.jpg';
 import nacadubaBeroe_xs from './../../../images/identity/butterfly/南方娜波灰蝶1.jpg';
 import prosotasDubiosa_xs from './../../../images/identity/butterfly/密紋波灰蝶1.jpg';
-import prosotasNora_ms from './../../../images/identity/butterfly/波灰蝶1.jpg';
 
-import acytolepisPuspa_ms from './../../../images/identity/butterfly/靛色琉灰蝶1.jpg';
 import celastrinaLavendularis_ms from './../../../images/identity/butterfly/細邊琉灰蝶1.jpg';
-import neopithecopsZalmora_fs from './../../../images/identity/butterfly/黑點灰蝶1.jpg';
-import megisbaMalaya_fs from './../../../images/identity/butterfly/黑星灰蝶1.jpg';
 import deudorixEpijarbas_xs from './../../../images/identity/butterfly/玳灰蝶1.jpg';
-import rapalaVaruna_xs from './../../../images/identity/butterfly/燕灰蝶1.jpg';
 
 export const lycaenidae = [
-    {
-		"name_chi": "藍灰蝶", // 中文俗名
-		"name_latin": "Zizeeria maha okinawana", // 拉丁學名
-		"sex": "♂", // 性別
+	{
+		"name_chi": "銀灰蝶", // 中文俗名
+		"name_latin": "Curetis acuta formosana Fruhstorfer", // 拉丁學名
+		"sex": "", // 性別
 		"direction": "s", // 觀察方向：head=h, tail=t, side=s
-		"feature": "藍灰蝶後翅中央的小斑紋排成環形，折列藍灰蝶則因為從前往後數來的第二個斑紋向內偏移而使這些小斑排成兩列；莧藍灰蝶的翅背面藍色的鱗片顏色較深，而且前翅中央的斑列顏色特別深。", // 辨識重點
-		"href": "http://dearlep.tw/species.html?namecode=345798&photo_id=7d56bda86cd86e72ca85921d2b19e47c", // Source Path
-		"image": pseudozizeeriaMaha_ms, // 在地照片
+		"feature": "", // 辨識重點
+		"href": "http://dearlep.tw/species.html?namecode=345717&photo_id=36aa23b44babad2d67f1407c7cb34b9c", // Source Path
+		"image": curetisAcuta_xs, // 在地照片
 		"family_chi": "灰蝶科", // 中文科名
 		"family_latin": "Lycaenidae" // 拉丁科名
+	},
+	{
+		"name_chi": "紫日灰蝶",
+		"name_latin": "Heliophorus ila matsumurae",
+		"sex": "",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345734&photo_id=7bd7b55e340ea60ac610e66f8ec5c118",
+		"image": heliophorusIla_xs,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+	{
+		"name_chi": "燕尾紫灰蝶",
+		"name_latin": "Arhopala bazalus turbata",
+		"sex": "",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345689&photo_id=07095f6329b500b4eb3549bf2a81f965",
+		"image": arhopalaBazalus_xs,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+	{
+		"name_chi": "日本紫灰蝶",
+		"name_latin": "Arhopala japonica",
+		"sex": "",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345692&photo_id=c26874bcd817f37d478053d54db0c266",
+		"image": arhopalaJaponica_xs,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+	{
+		"name_chi": "凹翅紫灰蝶",
+		"name_latin": "Mahathala ameria hainani Bethune-Baker",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=428839&photo_id=9bed5f36948dd4cf728cc17716ae5389",
+		"image": mahathalaAmeria_ms,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+	{
+		"name_chi": "綠灰蝶",
+		"name_latin": "Artipe eryx horiella",
+		"sex": "",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345695&photo_id=d90fcc180512c3edb8ac5170ad2ed1bd",
+		"image": artipeEryx_xs,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+	{
+		"name_chi": "燕灰蝶",
+		"name_latin": "Rapala varuna formosana Fruhstorfer",
+		"sex": "",
+		"direction": "s",
+		"feature": "波紋較細，內外異色，白邊不明顯，假眼往後看。",
+		"href": "http://dearlep.tw/species.html?namecode=345768&photo_id=26e2a964e4d4997cea09b7b5288cf250",
+		"image": rapalaVaruna_xs,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+	{
+		"name_chi": "三斑虎灰蝶",
+		"name_latin": "Spindasis syama lamuae Hsu & Liang",
+		"sex": "♀",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345776&photo_id=bbeeb63181694130ffd1f3aa067a018f",
+		"image": spindasisSyama_fs,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+	{
+		"name_chi": "虎灰蝶",
+		"name_latin": "Spindasis lohita formosanus",
+		"sex": "♀",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=437885&photo_id=8eed13c753dcb0f46a2b00a23e8f41d5",
+		"image": spindasisLohita_fs,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+	{
+		"name_chi": "大娜波灰蝶",
+		"name_latin": "Nacaduba kurava therasia",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "有尾突，前翅三條波狀紋，靠外緣波狀紋較『平整』。波紋白線明顯。",
+		"href": "http://dearlep.tw/species.html?namecode=345753&photo_id=ececc2eef664fc6715ce25f870684929",
+		"image": nacadubaKurava_ms,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+	{
+		"name_chi": "波灰蝶",
+		"name_latin": "Prosotas nora formosana",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "有尾突，前翅三條『金色』波狀紋，靠外緣波狀紋凹凸不平，第四小段排列歪斜。波紋白線不明顯，白線內黑邊明顯。",
+		"href": "http://dearlep.tw/species.html?namecode=345764&photo_id=2569c5fa1c4dd45b9031b2f7595f310e",
+		"image": prosotasNora_ms,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+	{
+		"name_chi": "雅波灰蝶",
+		"name_latin": "Jamides bochus formosanus Fruhstorfer",
+		"sex": "♀♂",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345743&photo_id=6894dc9f67a08aab8ae6e0de42740cb6",
+		"image": jamidesBochus_mfs,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+	{
+		"name_chi": "淡青雅波灰蝶",
+		"name_latin": "Jamides alecto dromicus Fruhstorfer",
+		"sex": "♀",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345742&photo_id=dbdc9fa5a67a517697a709c1ff58b748",
+		"image": jamidesAlecto_fs,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+	{
+		"name_chi": "藍丸灰蝶",
+		"name_latin": "Pithecops fulgens urai Bethune-Baker",
+		"sex": "",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345763&photo_id=c034ef7bd79aaae7b73a3c54e380f1bf",
+		"image": pithecopsFulgens_xs,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+	{
+		"name_chi": "黑點灰蝶",
+		"name_latin": "Neopithecops zalmora",
+		"sex": "♀",
+		"direction": "s",
+		"feature": "後翅基部無黑點，無尾突。",
+		"href": "http://dearlep.tw/species.html?namecode=345755&photo_id=be1d8b58f11b7ad0c662143263f9a8b7",
+		"image": neopithecopsZalmora_fs,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+	{
+		"name_chi": "黑星灰蝶",
+		"name_latin": "Megisba malaya sikkima Moore",
+		"sex": "♀",
+		"direction": "s",
+		"feature": "後翅基部有黑點，有尾突。",
+		"href": "http://dearlep.tw/species.html?namecode=345750&photo_id=95355d80823af0563dfd2a644e8dba05",
+		"image": megisbaMalaya_fs,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+	{
+		"name_chi": "靛色琉灰蝶",
+		"name_latin": "Acytolepis puspa myla",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "前翅 3+2 黑色點，後翅上外緣兩點底色同。",
+		"href": "http://dearlep.tw/species.html?namecode=431963&photo_id=0ad79dfe9c44f0e7838875a277d36f65",
+		"image": acytolepisPuspa_ms,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
+	},
+    {
+		"name_chi": "藍灰蝶",
+		"name_latin": "Zizeeria maha okinawana",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "藍灰蝶後翅中央的小斑紋排成環形，折列藍灰蝶則因為從前往後數來的第二個斑紋向內偏移而使這些小斑排成兩列；莧藍灰蝶的翅背面藍色的鱗片顏色較深，而且前翅中央的斑列顏色特別深。",
+		"href": "http://dearlep.tw/species.html?namecode=345798&photo_id=7d56bda86cd86e72ca85921d2b19e47c",
+		"image": pseudozizeeriaMaha_ms,
+		"family_chi": "灰蝶科",
+		"family_latin": "Lycaenidae"
 	},
 	{
 		"name_chi": "折列藍灰蝶",
@@ -62,17 +261,6 @@ export const lycaenidae = [
 		"family_latin": "Lycaenidae"
 	},
 	{
-		"name_chi": "大娜波灰蝶",
-		"name_latin": "Nacaduba kurava therasia",
-		"sex": "♂",
-		"direction": "s",
-		"feature": "有尾突，前翅三條波狀紋，靠外緣波狀紋較『平整』。波紋白線明顯。",
-		"href": "http://dearlep.tw/species.html?namecode=345753&photo_id=ececc2eef664fc6715ce25f870684929",
-		"image": nacadubaKurava_ms,
-		"family_chi": "灰蝶科",
-		"family_latin": "Lycaenidae"
-	},
-	{
 		"name_chi": "南方娜波灰蝶",
 		"name_latin": "Nacaduba beroe asakusa",
 		"sex": "",
@@ -95,28 +283,6 @@ export const lycaenidae = [
 		"family_latin": "Lycaenidae"
 	},
 	{
-		"name_chi": "波灰蝶",
-		"name_latin": "Prosotas nora formosana",
-		"sex": "♂",
-		"direction": "s",
-		"feature": "有尾突，前翅三條『金色』波狀紋，靠外緣波狀紋凹凸不平，第四小段排列歪斜。波紋白線不明顯，白線內黑邊明顯。",
-		"href": "http://dearlep.tw/species.html?namecode=345764&photo_id=2569c5fa1c4dd45b9031b2f7595f310e",
-		"image": prosotasNora_ms,
-		"family_chi": "灰蝶科",
-		"family_latin": "Lycaenidae"
-	},
-	{
-		"name_chi": "靛色琉灰蝶",
-		"name_latin": "Acytolepis puspa myla",
-		"sex": "♂",
-		"direction": "s",
-		"feature": "前翅 3+2 黑色點，後翅上外緣兩點底色同。",
-		"href": "http://dearlep.tw/species.html?namecode=431963&photo_id=0ad79dfe9c44f0e7838875a277d36f65",
-		"image": acytolepisPuspa_ms,
-		"family_chi": "灰蝶科",
-		"family_latin": "Lycaenidae"
-	},
-	{
 		"name_chi": "細邊琉灰蝶",
 		"name_latin": "Celastrina lavendularis himilcon",
 		"sex": "♂",
@@ -128,28 +294,6 @@ export const lycaenidae = [
 		"family_latin": "Lycaenidae"
 	},
 	{
-		"name_chi": "黑點灰蝶",
-		"name_latin": "Neopithecops zalmora",
-		"sex": "♀",
-		"direction": "s",
-		"feature": "後翅基部無黑點，無尾突。",
-		"href": "http://dearlep.tw/species.html?namecode=345755&photo_id=be1d8b58f11b7ad0c662143263f9a8b7",
-		"image": neopithecopsZalmora_fs,
-		"family_chi": "灰蝶科",
-		"family_latin": "Lycaenidae"
-	},
-	{
-		"name_chi": "黑星灰蝶",
-		"name_latin": "Megisba malaya sikkima Moore",
-		"sex": "♀",
-		"direction": "s",
-		"feature": "後翅基部有黑點，有尾突。",
-		"href": "http://dearlep.tw/species.html?namecode=345750&photo_id=95355d80823af0563dfd2a644e8dba05",
-		"image": megisbaMalaya_fs,
-		"family_chi": "灰蝶科",
-		"family_latin": "Lycaenidae"
-	},
-	{
 		"name_chi": "玳灰蝶",
 		"name_latin": "Deudorix epijarbas menesicles Fruhstorfer",
 		"sex": "",
@@ -157,17 +301,6 @@ export const lycaenidae = [
 		"feature": "波紋較寬，內外同色，白邊明顯，假眼直瞪。",
 		"href": "http://dearlep.tw/species.html?namecode=345720&photo_id=05375b08e7a084e8b56b60ad86bee5fc",
 		"image": deudorixEpijarbas_xs,
-		"family_chi": "灰蝶科",
-		"family_latin": "Lycaenidae"
-	},
-	{
-		"name_chi": "燕灰蝶",
-		"name_latin": "Rapala varuna formosana Fruhstorfer",
-		"sex": "",
-		"direction": "s",
-		"feature": "波紋較細，內外異色，白邊不明顯，假眼往後看。",
-		"href": "http://dearlep.tw/species.html?namecode=345768&photo_id=26e2a964e4d4997cea09b7b5288cf250",
-		"image": rapalaVaruna_xs,
 		"family_chi": "灰蝶科",
 		"family_latin": "Lycaenidae"
 	}
