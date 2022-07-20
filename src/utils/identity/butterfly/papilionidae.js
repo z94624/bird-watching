6e93094a0a98f3ef51064682157231f3047d5962
smoBEE <z94624@gmail.com>
2022-07-20 20:56:16 +0800

    Identity: 粉蝶科 與 鳳蝶科 必考新增完成。

--- src/utils/identity/butterfly/papilionidae.js
+++ src/utils/identity/butterfly/papilionidae.js
@@ -1,4 +1,220 @@
 // 鳳蝶科
-export const papilionidae = [
+import byasaConfusus_mh from './../../../images/identity/butterfly/麝鳳蝶1.jpg';
+import byasaImpediens_fs from './../../../images/identity/butterfly/長尾麝鳳蝶1.jpg';
+import byasaPolyeuctes_mh from './../../../images/identity/butterfly/多姿麝鳳蝶1.jpg';
+import graphiumEurous_ms from './../../../images/identity/butterfly/劍鳳蝶1.jpg';
+import pachlioptaAristolochiae_fs from './../../../images/identity/butterfly/紅珠鳳蝶1.jpg';
+import graphiumSarpedon_ms from './../../../images/identity/butterfly/青鳳蝶1.jpg';
+import graphiumCloanthus_ms from './../../../images/identity/butterfly/寬帶青鳳蝶1.jpg';
+import graphiumDoson_ms from './../../../images/identity/butterfly/木蘭青鳳蝶1.jpg';
+import papilioDemoleus_mfs from './../../../images/identity/butterfly/花鳳蝶1.jpg';
+import papilioProtenor_ms from './../../../images/identity/butterfly/黑鳳蝶1.jpg';
+import papilioMemnon_ms from './../../../images/identity/butterfly/大鳳蝶1.jpg';
+import papilioPolytes_fh from './../../../images/identity/butterfly/玉帶鳳蝶1.jpg';
+import papilioThaiwanus_fh from './../../../images/identity/butterfly/臺灣鳳蝶1.jpg';
+import papilioHelenus_xs from './../../../images/identity/butterfly/白紋鳳蝶1.jpg';
+import papilioNephelus_ms from './../../../images/identity/butterfly/大白紋鳳蝶1.jpg';
+import papilioCastor_mh from './../../../images/identity/butterfly/無尾白紋鳳蝶1.jpg';
+import papilioBianor_mh from './../../../images/identity/butterfly/翠鳳蝶1.jpg';
+import papilioParis_mh from './../../../images/identity/butterfly/琉璃翠鳳蝶1.jpg';
 
+export const papilionidae = [
+    {
+		"name_chi": "麝鳳蝶", // 中文俗名
+		"name_latin": "Byasa confusus mansonensis", // 拉丁學名
+		"sex": "♂", // 性別
+		"direction": "h", // 觀察方向：head=h, tail=t, side=s
+		"feature": "", // 辨識重點
+		"href": "http://dearlep.tw/species.html?namecode=434529&photo_id=1dbd3f502eb8b165b24a7a2930f9629d", // Source Path
+		"image": byasaConfusus_mh, // 在地照片
+		"family_chi": "鳳蝶科", // 中文科名
+		"family_latin": "Papilionidae" // 拉丁科名
+	},
+    {
+		"name_chi": "長尾麝鳳蝶",
+		"name_latin": "Byasa impediens febanus",
+		"sex": "♀",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347273&photo_id=89c7c1eedfd6e16449c846239d3d7d71",
+		"image": byasaImpediens_fs,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "多姿麝鳳蝶",
+		"name_latin": "Byasa polyeuctes termessus",
+		"sex": "♂",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347274&photo_id=b2012689d49512fa7f3f3732ae7fdad2",
+		"image": byasaPolyeuctes_mh,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "劍鳳蝶",
+		"name_latin": "Graphium eurous asakurae",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=434519&photo_id=37b3d3fd7b40defc82a08c78b2ab8959",
+		"image": graphiumEurous_ms,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "紅珠鳳蝶",
+		"name_latin": "Pachliopta aristolochiae interposita",
+		"sex": "♀",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347283&photo_id=8417670658dd1cfe1cd3a75fa5e85b11",
+		"image": pachlioptaAristolochiae_fs,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "青鳳蝶",
+		"name_latin": "Graphium sarpedon connectens",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347281&photo_id=ccfe219143281a14089c9b541b759f9a",
+		"image": graphiumSarpedon_ms,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "寬帶青鳳蝶",
+		"name_latin": "Graphium cloanthus kuge",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347279&photo_id=d7e82d8ae29689296b473924b7e140fc",
+		"image": graphiumCloanthus_ms,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "木蘭青鳳蝶",
+		"name_latin": "Graphium doson postianus",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347280&photo_id=b113069f30ee9d209800401283d7f218",
+		"image": graphiumDoson_ms,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "花鳳蝶",
+		"name_latin": "Papilio demoleus Linnaeus",
+		"sex": "♀♂",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347287&photo_id=36af97bb6e17544818b8b0db2b73658b",
+		"image": papilioDemoleus_mfs,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "黑鳳蝶",
+		"name_latin": "Papilio protenor protenor Cramer",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347299&photo_id=0d437220f9f44c249bb5c65515d941a3",
+		"image": papilioProtenor_ms,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "大鳳蝶",
+		"name_latin": "Papilio memnon heronus Fruhstorfer",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347293&photo_id=42f64036915298968b6ba197535f6e03",
+		"image": papilioMemnon_ms,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "玉帶鳳蝶",
+		"name_latin": "Papilio polytes polytes Linnaeus",
+		"sex": "♀",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347298&photo_id=dc6eafa91d384a328721f882c1a73712",
+		"image": papilioPolytes_fh,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "臺灣鳳蝶",
+		"name_latin": "Papilio thaiwanus Rothschild",
+		"sex": "♀",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347301&photo_id=a778c98ea864ac952be231705e3b0e43",
+		"image": papilioThaiwanus_fh,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "白紋鳳蝶",
+		"name_latin": "Papilio helenus fortunius Fruhstorfer",
+		"sex": "",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347289&photo_id=a81abb80f01254376c1651b9ef3cfe62",
+		"image": papilioHelenus_xs,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "大白紋鳳蝶",
+		"name_latin": "Papilio nephelus chaonulus Fruhstorfer",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347294&photo_id=2b146cc1281916b9fe860192e82f4c9b",
+		"image": papilioNephelus_ms,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "無尾白紋鳳蝶",
+		"name_latin": "Papilio castor formosanus Rothschild",
+		"sex": "♂",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347286&photo_id=e8b1681aee3bb02ff525fe323f25c66e",
+		"image": papilioCastor_mh,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "翠鳳蝶",
+		"name_latin": "Papilio bianor thrasymedes Fruhstorfer",
+		"sex": "♂",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347285&photo_id=9f48ff03deefeff33c3393c9c2ed7996",
+		"image": papilioBianor_mh,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	},
+    {
+		"name_chi": "琉璃翠鳳蝶",
+		"name_latin": "Papilio paris nakaharai Shirôzu",
+		"sex": "♂",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347295&photo_id=1c16976054744961d0352b7b454d66e7",
+		"image": papilioParis_mh,
+		"family_chi": "鳳蝶科",
+		"family_latin": "Papilionidae"
+	}
 ];
--- src/utils/identity/butterfly/pieridae.js
+++ src/utils/identity/butterfly/pieridae.js
@@ -1,4 +1,76 @@
 // 粉蝶科
-export const pieridae = [
+import pierisRapae_xs from './../../../images/identity/butterfly/白粉蝶1.jpg';
+import pierisCanidia_xs from './../../../images/identity/butterfly/緣點白粉蝶1.jpg';
+import appiasLyncida_xs from './../../../images/identity/butterfly/異色尖粉蝶1.jpg';
+import leptosiaNina_ms from './../../../images/identity/butterfly/纖粉蝶1.jpg';
+import catopsiliaPomona_xs from './../../../images/identity/butterfly/遷粉蝶1.jpg';
+import hebomoiaGlaucippe_ms from './../../../images/identity/butterfly/橙端粉蝶1.jpg';
 
+export const pieridae = [
+    {
+		"name_chi": "白粉蝶", // 中文俗名
+		"name_latin": "Pieris rapae crucivora Boisduval", // 拉丁學名
+		"sex": "", // 性別
+		"direction": "s", // 觀察方向：head=h, tail=t, side=s
+		"feature": "", // 辨識重點
+		"href": "http://dearlep.tw/species.html?namecode=347350&photo_id=331822cc8e65a82338f9e0c00ca4843f", // Source Path
+		"image": pierisRapae_xs, // 在地照片
+		"family_chi": "粉蝶科", // 中文科名
+		"family_latin": "Pieridae" // 拉丁科名
+	},
+    {
+		"name_chi": "緣點白粉蝶",
+		"name_latin": "Pieris canidia",
+		"sex": "",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347349&photo_id=20e7a54ad02393cba3d7c0db4361a9ae",
+		"image": pierisCanidia_xs,
+		"family_chi": "粉蝶科",
+		"family_latin": "Pieridae"
+	},
+    {
+		"name_chi": "異色尖粉蝶",
+		"name_latin": "Appias lyncida eleonora",
+		"sex": "",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347323&photo_id=30a2cb3241931bf3abded221ecabae31",
+		"image": appiasLyncida_xs,
+		"family_chi": "粉蝶科",
+		"family_latin": "Pieridae"
+	},
+    {
+		"name_chi": "纖粉蝶",
+		"name_latin": "Leptosia nina niobe",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347348&photo_id=dd4d2fcb964a1ee657680244e35e9077",
+		"image": leptosiaNina_ms,
+		"family_chi": "粉蝶科",
+		"family_latin": "Pieridae"
+	},
+    {
+		"name_chi": "遷粉蝶",
+		"name_latin": "Catopsilia pomona pomona",
+		"sex": "",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347327&photo_id=2d787f0150145d258ca36f892f5f28ef",
+		"image": catopsiliaPomona_xs,
+		"family_chi": "粉蝶科",
+		"family_latin": "Pieridae"
+	},
+    {
+		"name_chi": "橙端粉蝶",
+		"name_latin": "Hebomoia glaucippe formosana Fruhstorfer",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=347346&photo_id=38696c7af7623a0ca5e1f9ec0e6405d5",
+		"image": hebomoiaGlaucippe_ms,
+		"family_chi": "粉蝶科",
+		"family_latin": "Pieridae"
+	}
 ];
