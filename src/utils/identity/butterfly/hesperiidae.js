5126c868a5d4dad619295c83ef106ed556a9ef84
smoBEE <z94624@gmail.com>
2022-07-21 14:23:19 +0800

    Identity: 新增必考 弄蝶科。

--- src/utils/identity/butterfly/hesperiidae.js
+++ src/utils/identity/butterfly/hesperiidae.js
@@ -1,4 +1,148 @@
 // 弄蝶科
-export const hesperiidae = [
+import hasoraBadra_xs from './../../../images/identity/butterfly/鐵色絨弄蝶1.jpg';
+import buraraJaina_fs from './../../../images/identity/butterfly/橙翅傘弄蝶1.jpg';
+import choaspesBenjaminii_ms from './../../../images/identity/butterfly/綠弄蝶1.jpg';
+import tagiadesTethys_xh from './../../../images/identity/butterfly/玉帶弄蝶1.jpg';
+import tagiadesCohaerens_xh from './../../../images/identity/butterfly/白裙弄蝶1.jpg';
+import seseriaFormosana_xh from './../../../images/identity/butterfly/臺灣瑟弄蝶1.jpg';
+import isoteinonLamprospilus_ms from './../../../images/identity/butterfly/白斑弄蝶1.jpg';
+import notocryptaCurvifascia_xh from './../../../images/identity/butterfly/袖弄蝶1.jpg';
+import udaspesFolus_xs from './../../../images/identity/butterfly/薑弄蝶1.jpg';
+import suastusGremius_xs from './../../../images/identity/butterfly/黑星弄蝶1.jpg';
+import borboCinnara_xs from './../../../images/identity/butterfly/禾弄蝶1.jpg';
+import caltorisRanrunna_xh from './../../../images/identity/butterfly/黯弄蝶1.jpg';
 
+export const hesperiidae = [
+    {
+		"name_chi": "鐵色絨弄蝶", // 中文俗名
+		"name_latin": "Hasora badra", // 拉丁學名
+		"sex": "", // 性別
+		"direction": "s", // 觀察方向：head=h, tail=t, side=s
+		"feature": "", // 辨識重點
+		"href": "http://dearlep.tw/species.html?namecode=345474&photo_id=b6601e8e196334a58bda6dd51b2c038e", // Source Path
+		"image": hasoraBadra_xs, // 在地照片
+		"family_chi": "弄蝶科", // 中文科名
+		"family_latin": "Hesperiidae" // 拉丁科名
+	},
+    {
+		"name_chi": "橙翅傘弄蝶",
+		"name_latin": "Burara jaina formosana",
+		"sex": "♀",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345456&photo_id=07b444a51784b589ac568dabd024e4c4",
+		"image": buraraJaina_fs,
+		"family_chi": "弄蝶科",
+		"family_latin": "Hesperiidae"
+	},
+    {
+		"name_chi": "綠弄蝶",
+		"name_latin": "Choaspes benjaminii formosanus",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345466&photo_id=dd24b567a7be1d3e7fec25bd99df5071",
+		"image": choaspesBenjaminii_ms,
+		"family_chi": "弄蝶科",
+		"family_latin": "Hesperiidae"
+	},
+    {
+		"name_chi": "玉帶弄蝶",
+		"name_latin": "Tagiades tethys moori",
+		"sex": "",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=429380&photo_id=45d9272de22c0eb412dad65e5bb4a49e",
+		"image": tagiadesTethys_xh,
+		"family_chi": "弄蝶科",
+		"family_latin": "Hesperiidae"
+	},
+    {
+		"name_chi": "白裙弄蝶",
+		"name_latin": "Tagiades cohaerens Mabille",
+		"sex": "",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345506&photo_id=041be935a060f9ff2311c94aed83acfd",
+		"image": tagiadesCohaerens_xh,
+		"family_chi": "弄蝶科",
+		"family_latin": "Hesperiidae"
+	},
+    {
+		"name_chi": "臺灣瑟弄蝶",
+		"name_latin": "Seseria formosana",
+		"sex": "",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345504&photo_id=e2eb62e077c4509e568de4330f7afca3",
+		"image": seseriaFormosana_xh,
+		"family_chi": "弄蝶科",
+		"family_latin": "Hesperiidae"
+	},
+    {
+		"name_chi": "白斑弄蝶",
+		"name_latin": "Isoteinon lamprospilus formosanus Fruhstorfer",
+		"sex": "♂",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345477&photo_id=f6c9a347971a8418ccf1982d82cddbbd",
+		"image": isoteinonLamprospilus_ms,
+		"family_chi": "弄蝶科",
+		"family_latin": "Hesperiidae"
+	},
+    {
+		"name_chi": "袖弄蝶",
+		"name_latin": "Notocrypta curvifascia",
+		"sex": "",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345479&photo_id=7ebba328cfb60af19a352b482eeb38f6",
+		"image": notocryptaCurvifascia_xh,
+		"family_chi": "弄蝶科",
+		"family_latin": "Hesperiidae"
+	},
+    {
+		"name_chi": "薑弄蝶",
+		"name_latin": "Udaspes folus",
+		"sex": "",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345512&photo_id=6a18a3c6c18c3267bea1eaebb003e957",
+		"image": udaspesFolus_xs,
+		"family_chi": "弄蝶科",
+		"family_latin": "Hesperiidae"
+	},
+    {
+		"name_chi": "黑星弄蝶",
+		"name_latin": "Suastus gremius",
+		"sex": "",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345505&photo_id=646f636a8591dba9f4437fdd623fad30",
+		"image": suastusGremius_xs,
+		"family_chi": "弄蝶科",
+		"family_latin": "Hesperiidae"
+	},
+    {
+		"name_chi": "禾弄蝶",
+		"name_latin": "Borbo cinnara",
+		"sex": "",
+		"direction": "s",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=345455&photo_id=ce0fb4965e3292800999e9c57ad1aa5d",
+		"image": borboCinnara_xs,
+		"family_chi": "弄蝶科",
+		"family_latin": "Hesperiidae"
+	},
+    {
+		"name_chi": "黯弄蝶",
+		"name_latin": "Caltoris ranrunna Sonan",
+		"sex": "",
+		"direction": "h",
+		"feature": "",
+		"href": "http://dearlep.tw/species.html?namecode=413370&photo_id=880724c5a692d8a0697e1d285fb475d2",
+		"image": caltorisRanrunna_xh,
+		"family_chi": "弄蝶科",
+		"family_latin": "Hesperiidae"
+	}
 ];
